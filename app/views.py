a9eb791a75b460b37976077f70998cdeea14d2e9
seolhyebom <seolhyebom@gmail.com>
2024-03-19 01:39:14 +0900

    사이트 전체적으로 수정작업

--- app/views.py
+++ app/views.py
@@ -1,95 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from study.settings import BASE_DIR   # Django settings.py 파일에서 BASE_DIR을 가져옴
-from sklearn.preprocessing import PolynomialFeatures    # 다항 특성을 만들기 위한 모듈
-from sklearn.linear_model import LinearRegression   # 선형 회귀 모델
 from pathlib import Path    # 파일 경로를 다루기 위한 모듈
 from datetime import date   # 현재 날짜를 얻기 위한 모듈
-from .forms import ForecastForm   # 예측 폼을 가져옴
-import pandas as pd # 데이터프레임을 다루기 위한 모듈
-
-# data.csv 파일을 읽어들여 DataFrame 객체로 저장
-DF_MAIN = pd.read_csv(BASE_DIR / 'data.csv', encoding='cp949')
-
-# 각 에너지 사용량에 대한 예측을 계산하는 함수
-def calculate_prediction(X, y, year):
-    poly_features = PolynomialFeatures(degree=3, include_bias=False)
-    X_poly = poly_features.fit_transform(X.reshape(-1, 1))
-    lin_reg = LinearRegression()
-    lin_reg.fit(X_poly, y)
-    r2 = lin_reg.score(X_poly, y)
-    r2_percent = r2 * 100
-    predictions = lin_reg.predict(poly_features.transform([[year]]))
-    
-    # 예측값이 음수일 경우 표시하는 에러 메시지
-    if predictions[0] < 0:
-        predicted_message = "유효한 예측값이 아닙니다." 
-        r2_percent = "측정불가"
-
-    # 예측값이 양수일 경우 표시하는 메시지
-    else:
-        predicted_message = f'{predictions[0]:,.2f}'
-        r2_percent = f'{r2_percent:.2f}'
-
-    return predicted_message, r2_percent
 
 
-# 웹 애플리케이션의 루트 URL에 접속했을 때 'index.html' 템플릿을 보여주는 역할
-def index(request: HttpRequest) -> HttpResponse:
-    data = {}
 
-    if request.method == 'POST':
-        # POST 요청에서 폼 데이터 추출
-        form = ForecastForm(request.POST)
 
-        if form.is_valid():
-            year = form.cleaned_data['year']
-            month = int(form.cleaned_data['month'])
-            district = form.cleaned_data['district']
-            town = form.cleaned_data['town']
 
-            # 전기 사용량 데이터 처리
-            df_elec = DF_MAIN[(DF_MAIN['월'] == month) & (DF_MAIN['구'] == district) & (DF_MAIN['동'] == town)][['년도', '전기']]
-            X_elec = df_elec['년도'].values
-            y_elec = df_elec['전기'].values
-            data['electric'], r2_percent_elec = calculate_prediction(X_elec, y_elec, year)
 
-            # 수도 사용량 데이터 처리
-            df_water = DF_MAIN[(DF_MAIN['월'] == month) & (DF_MAIN['구'] == district) & (DF_MAIN['동'] == town)][['년도', '수도']]
-            X_water = df_water['년도'].values
-            y_water = df_water['수도'].values
-            data['water'], r2_percent_water = calculate_prediction(X_water, y_water, year)
 
-            # 도시가스 사용량 데이터 처리
-            df_gas = DF_MAIN[(DF_MAIN['월'] == month) & (DF_MAIN['구'] == district) & (DF_MAIN['동'] == town)][['년도', '가스']]
-            X_gas = df_gas['년도'].values
-            y_gas = df_gas['가스'].values
-            data['gas'], r2_percent_gas = calculate_prediction(X_gas, y_gas, year)
+def index(request: HttpRequest) -> HttpResponse: # 메인 페이지
+    return render(request, 'study/index.html')
 
-            # 탄소 사용량 데이터 처리
-            df_carbon = DF_MAIN[(DF_MAIN['월'] == month) & (DF_MAIN['구'] == district) & (DF_MAIN['동'] == town)][['년도', '탄소']]
-            X_carbon = df_carbon['년도'].values
-            y_carbon = df_carbon['탄소'].values
-            data['carbon'], r2_percent_carbon = calculate_prediction(X_carbon, y_carbon, year)
 
-            data['comment'] = f'{year}년 {month}월 서울시 {district} {town} 에너지 사용 예측량입니다.'
-
-            form = ForecastForm()
-    elif request.method == 'GET':
-        # GET 요청인 경우 처리
-        form = ForecastForm()
-
-    context = {
-        'date': date.today().isoformat(),
-        'form': form,
-        'comment': "현재 날짜보다 예측 년도/월이 높아야 합니다." if form.errors else data.get('comment', '예측 하기 버튼을 눌러주세요.'),
-        'electric': data.get('electric', '-'),
-        'water': data.get('water', '-'),
-        'gas': data.get('gas', '-'),
-        'carbon': data.get('carbon', '-'),
-    }
-
-    return render(request, 'study/index.html', context)
 
 def signup(request: HttpRequest) -> HttpResponse: # 회원가입 페이지
     return render(request, 'study/signup.html')
--- user/views.py
+++ user/views.py
@@ -48,29 +48,128 @@ def signup_view(request):
 
 
 
+# @login_required
+# def lecture_list_view(request):
+#     user = request.user  # 현재 로그인한 사용자 정보 가져오기
+#     chapters = LectureChapter.objects.filter(user=user)
+
+#     context = {
+#         'chapters': chapters,  # 사용자의 LectureChapter 객체 목록을 전달
+#     }
+
+#     return render(request, "user/lecture_list.html", context)
+
 @login_required
 def lecture_list_view(request):
-    user = request.user  # 현재 로그인한 사용자 정보 가져오기
-    chapters = LectureChapter.objects.filter(user=user)
+    user = request.user
+    lecture_chapters = LectureChapter.objects.filter(user=user).select_related('lecture').order_by('lecture__title')
+
+    lectures = []
+    current_lecture = None
+    for chapter in lecture_chapters:
+        if current_lecture != chapter.lecture.title:
+            lectures.append({'lecture': chapter.lecture.title, 'chapters': []})
+            current_lecture = chapter.lecture.title
+        lectures[-1]['chapters'].append(chapter.chapter_name)
 
     context = {
-        'chapters': chapters,  # 사용자의 LectureChapter 객체 목록을 전달
+        'lectures': lectures,
     }
-
     return render(request, "user/lecture_list.html", context)
 
 
 
+
+
+
+
+
+# @login_required
+# def lecture_view(request):
+#     user = request.user  # 현재 로그인한 사용자 정보 가져오기
+#     chapters = LectureChapter.objects.filter(user=user)
+
+#     context = {
+#         'chapters': chapters,  # 사용자의 LectureChapter 객체 목록을 전달
+#     }
+
+#     return render(request, "user/lecture.html", context)
+
 @login_required
 def lecture_view(request):
-    user = request.user  # 현재 로그인한 사용자 정보 가져오기
-    chapters = LectureChapter.objects.filter(user=user)
+    user = request.user
+    lecture_chapters = LectureChapter.objects.filter(user=user).select_related('lecture').order_by('lecture__title')
+
+    lectures = []
+    current_lecture = None
+    for chapter in lecture_chapters:
+        if current_lecture != chapter.lecture.title:
+            lectures.append({'lecture': chapter.lecture.title, 'chapters': []})
+            current_lecture = chapter.lecture.title
+        lectures[-1]['chapters'].append(chapter.chapter_name)
 
     context = {
-        'chapters': chapters,  # 사용자의 LectureChapter 객체 목록을 전달
+        'lectures': lectures,
     }
-
     return render(request, "user/lecture.html", context)
+
+
+
+
+
+
+
+
+
+
+
+# @login_required
+# def add_lecture_chapter_view(request):
+#     if request.method == 'POST':
+#         form = LectureChapterForm(request.POST)
+#         if form.is_valid():
+#             # 폼에서 입력한 데이터 가져오기
+#             lecture_name = form.cleaned_data.get('lecture_name')
+#             chapter_name = form.cleaned_data.get('chapter_name')
+            
+#             # 터미널에 출력
+#             print("User:", request.user)
+#             print("Lecture:", lecture_name)
+#             print("Chapter Name:", chapter_name)
+#             print("성공적으로 추가되었습니다.")
+
+#             # Lecture 모델에서 해당 강의를 가져오거나 생성
+#             lecture, created = Lecture.objects.get_or_create(title=lecture_name)
+
+#             # 새로운 LectureChapter 객체 생성 및 저장
+#             chapter = form.save(commit=False)
+#             chapter.user = request.user
+#             chapter.lecture = lecture  # Lecture 객체 할당
+#             chapter.save()
+
+#             messages.success(request, '강의 챕터가 성공적으로 추가되었습니다.')
+#             return render(request, "user/add_lecture_chapter.html")
+#     else:
+#         form = LectureChapterForm()
+
+#     # 폼 유효성 검사 실패 시에도 폼과 함께 에러 메시지를 전달
+#     if form.errors:
+#         # 폼에서 입력한 데이터 가져오기
+#         lecture_name = form.cleaned_data.get('lecture_name')
+#         chapter_name = form.cleaned_data.get('chapter_name')
+        
+#         # 터미널에 출력
+#         print("User:", request.user)
+#         print("Lecture:", lecture_name)
+#         print("Chapter Name:", chapter_name)
+#         print("강의 챕터 추가에 실패했습니다.")
+
+#         messages.error(request, '강의 챕터 추가에 실패했습니다. 입력을 다시 확인해주세요.')
+
+#     return render(request, 'user/add_lecture_chapter.html')
+
+
+
 
 
 
@@ -83,20 +182,21 @@ def add_lecture_chapter_view(request):
             # 폼에서 입력한 데이터 가져오기
             lecture_name = form.cleaned_data.get('lecture_name')
             chapter_name = form.cleaned_data.get('chapter_name')
-            
-            # 터미널에 출력
-            print("User:", request.user)
-            print("Lecture:", lecture_name)
-            print("Chapter Name:", chapter_name)
 
-            # Lecture 모델에서 해당 강의를 가져오거나 생성
-            lecture, created = Lecture.objects.get_or_create(title=lecture_name)
+            # 현재 사용자의 lecture_name이 이미 존재하는지 확인
+            existing_lecture = LectureChapter.objects.filter(user=request.user, lecture__title=lecture_name).first()
 
-            # 새로운 LectureChapter 객체 생성 및 저장
-            chapter = form.save(commit=False)
-            chapter.user = request.user
-            chapter.lecture = lecture  # Lecture 객체 할당
-            chapter.save()
+            if existing_lecture:
+                # 해당 lecture_name이 이미 존재하는 경우
+                # lecture_name에 하위 목록 추가
+                LectureChapter.objects.create(user=request.user, lecture=existing_lecture.lecture, chapter_name=chapter_name)
+                print("기존 강의에 챕터를 추가하였습니다.")
+            else:
+                # 해당 lecture_name이 존재하지 않는 경우
+                # 새로운 강의 및 챕터 추가
+                lecture = Lecture.objects.create(title=lecture_name)
+                LectureChapter.objects.create(user=request.user, lecture=lecture, chapter_name=chapter_name)
+                print("새로운 강의 및 챕터를 추가하였습니다.")
 
             messages.success(request, '강의 챕터가 성공적으로 추가되었습니다.')
             return render(request, "user/add_lecture_chapter.html")
@@ -113,9 +213,9 @@ def add_lecture_chapter_view(request):
         print("User:", request.user)
         print("Lecture:", lecture_name)
         print("Chapter Name:", chapter_name)
+        print("강의 챕터 추가에 실패했습니다.")
+
         messages.error(request, '강의 챕터 추가에 실패했습니다. 입력을 다시 확인해주세요.')
+        print("강의 챕터 추가에 실패했습니다.")
 
     return render(request, 'user/add_lecture_chapter.html')
-
-
-
