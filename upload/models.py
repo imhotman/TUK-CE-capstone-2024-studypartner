bdd5a9ec6e1d3d8ac2d787118ff255e90b5c5fcf
seolhyebom <seolhyebom@gmail.com>
2024-03-25 02:14:31 +0900

    ㅌ

--- upload/models.py
+++ upload/models.py
@@ -10,8 +10,6 @@ class UploadFile(models.Model):
     file_name = models.FileField(null=True)
 
     def __str__(self):
-        lecture_title = self.lecture.title if self.lecture else 'No Lecture'
-        chapter_name = self.chapter.chapter_name if self.chapter else 'No Chapter'
         return f"사용자={self.user}, 강의명={self.lecture}, 챕터명={self.chapter}, 파일 제목={self.file_title}, 파일 이름={self.file_name}"
 
 
