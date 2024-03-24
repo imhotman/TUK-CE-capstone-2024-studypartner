b358cf9c0102637325a33a189446384f162d16b9
seolhyebom <seolhyebom@gmail.com>
2024-03-24 22:35:25 +0900

    업데이트

--- upload/views.py
+++ upload/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, Http404, redirect
 from django.contrib.auth.models import User
 from user.models import LectureChapter
@@ -5,9 +6,13 @@ from .models import UploadFile
 from .forms import UploadFileForm  # UploadFileForm을 가져옴
 
 
+
 def chapter_detail_view(request, lecture_name, chapter_name):
     # 강의명과 챕터명이 일치하는 LectureChapter 객체를 가져옴
     chapter = LectureChapter.objects.filter(lecture__title=lecture_name, chapter_name=chapter_name).first()
+
+    # 현재 로그인한 사용자 정보를 가져옴
+    current_user = request.user
 
     # LectureChapter가 없는 경우 404 에러 반환
     if not chapter:
@@ -39,6 +44,7 @@ def chapter_detail_view(request, lecture_name, chapter_name):
 
 
 
+@login_required
 def upload_file(request, lecture_name, chapter_name):
     # 강의명과 챕터명이 일치하는 LectureChapter 객체를 가져옴
     chapter = LectureChapter.objects.filter(lecture__title=lecture_name, chapter_name=chapter_name).first()
@@ -53,11 +59,15 @@ def upload_file(request, lecture_name, chapter_name):
             # 업로드된 파일의 lecture 필드를 해당 챕터의 강의로 설정
             upload_file = form.save(commit=False)
             upload_file.chapter = chapter
+
+            # 현재 로그인한 사용자 정보를 가져와서 업로드 파일의 user 필드에 설정
+            upload_file.user = request.user
+            
             upload_file.save()
 
             # 파일 업로드 성공 시 해당 챕터 세부 정보 페이지로 리디렉션
             print("파일 업로드 성공하였습니다.")
-            return redirect('chapter_detail', lecture_name=lecture_name, chapter_name=chapter_name)
+            return redirect('upload:chapter_detail', lecture_name=lecture_name, chapter_name=chapter_name)
         
     # 파일 업로드 실패 시 현재 페이지를 다시 렌더링하여 업로드 폼을 표시
     print("파일 업로드 실패하였습니다.")
@@ -65,4 +75,5 @@ def upload_file(request, lecture_name, chapter_name):
         'chapter': chapter,
         'form': UploadFileForm()  # 업로드 폼 생성
     }
+    print("모두 실패")
     return render(request, "upload/chapter_detail.html", context)
