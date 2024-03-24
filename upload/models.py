d53dcc62d61cb860bbb3cc76dffbb44a80d7a389
seolhyebom <seolhyebom@gmail.com>
2024-03-25 02:03:46 +0900

    x

--- upload/models.py
+++ upload/models.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
+from user.models import Lecture, LectureChapter
 
 class UploadFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    lecture_chapter = models.CharField(max_length=200, default="")
+    lecture = models.ForeignKey(Lecture, related_name='uploaded_files', on_delete=models.CASCADE, null=True)
+    chapter = models.ForeignKey(LectureChapter, on_delete=models.CASCADE, null=True)
     file_title = models.CharField(max_length=50, default="")
     file_name = models.FileField(null=True)
 
     def __str__(self):
-        return f"사용자={self.user}, 강의&챕터명={self.lecture_chapter}, 파일 제목={self.file_title}, 파일 이름={self.file_name}"
-
+        lecture_title = self.lecture.title if self.lecture else 'No Lecture'
+        chapter_name = self.chapter.chapter_name if self.chapter else 'No Chapter'
+        return f"사용자={self.user}, 강의명={lecture_title}, 챕터명={chapter_name}, 파일 제목={self.file_title}, 파일 이름={self.file_name}"
 
 
 
