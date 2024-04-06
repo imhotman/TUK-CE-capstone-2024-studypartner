b5829b1f6683806756de0511817e2654cba6bd97
seolhyebom <seolhyebom@gmail.com>
2024-04-06 22:50:20 +0900

    위치 수정

--- summary/urls.py
+++ summary/urls.py
@@ -6,6 +6,6 @@ app_name = "summary"
 urlpatterns = [
     # chapter에 대한 URL 패턴
     path('summary_detail/<str:lecture_name>/<str:chapter_name>/', views.summary_detail_view, name='summary_detail'),  # summary_detail
-    path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file'),
+    path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file_summary'),
 ]
 
