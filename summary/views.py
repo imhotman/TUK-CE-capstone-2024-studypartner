8552a90ab1f196a317231d7348db87254cc2e433
seolhyebom <seolhyebom@gmail.com>
2024-04-06 22:00:45 +0900

    일부 수정

--- summary/views.py
+++ summary/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, Http404, redirect
 from django.contrib.auth.models import User
 from user.models import Lecture, LectureChapter
-from .models import UploadFile
-from .forms import UploadFileForm  # UploadFileForm을 가져옴
+from .models import UploadFile_summary
+from .forms import UploadFile_summaryForm  # UploadFileForm을 가져옴
 
 # Create your views here.
 
@@ -21,10 +21,10 @@ def summary_view(request, lecture_name, chapter_name):
         raise Http404("챕터를 찾을 수 없습니다.")
 
     # 해당 챕터에 업로드된 파일들 가져오기
-    uploaded_files = UploadFile.objects.filter(chapter=chapter)
+    uploaded_files = UploadFile_summary.objects.filter(chapter=chapter)
 
     # 파일 업로드를 위한 폼 생성
-    form = UploadFileForm(request.POST or None, request.FILES or None)
+    form = UploadFile_summaryForm(request.POST or None, request.FILES or None)
 
     context = {
         'chapter': chapter,
@@ -38,31 +38,23 @@ def summary_view(request, lecture_name, chapter_name):
 
 def upload_file_summary(request, lecture_name, chapter_name):
     try:
-        # 강의명과 챕터명이 일치하는 LectureChapter 객체를 가져옴
         chapter = LectureChapter.objects.get(lecture__title=lecture_name, chapter_name=chapter_name)
     except LectureChapter.DoesNotExist:
         raise Http404("챕터를 찾을 수 없습니다.")
-
+    
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+        form = UploadFile_summaryForm(request.POST, request.FILES)
         if form.is_valid():
-            # 업로드된 파일의 lecture 및 chapter 필드를 해당 챕터의 강의 및 챕터로 설정
             upload_file = form.save(commit=False)
-            print(chapter)
-            upload_file.chapter = chapter # 강의와 챕터 이름 저장
-            upload_file.user = request.user  # 현재 로그인한 사용자 정보 할당
+            upload_file.chapter = chapter
+            upload_file.user = request.user
             upload_file.save()
-
-            # 파일 업로드 성공 시 해당 챕터 세부 정보 페이지로 이동
-            print("파일 업로드 성공하였습니다.")
             return redirect('summary:summary', lecture_name=lecture_name, chapter_name=chapter_name)
     else:
-        form = UploadFileForm()  # 업로드 폼 생성
-
-        # 파일 업로드 실패 시 현재 페이지를 다시 렌더링하여 업로드 폼을 표시
-        print("파일 업로드 실패하였습니다.")
-        context = {
-            'chapter': chapter,
-            'form': form
-        }
+        form = UploadFile_summaryForm()
+    
+    context = {
+        'chapter': chapter,
+        'form': form
+    }
     return render(request, "summary/summary.html", context)
