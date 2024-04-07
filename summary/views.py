7f89a58474c326fc41f869e18cba10e7cd266757
seolhyebom <seolhyebom@gmail.com>
2024-04-07 19:58:19 +0900

    summary_detail페이지 수정

--- summary/views.py
+++ summary/views.py
@@ -5,8 +5,36 @@ from django.contrib.auth.models import User
 from user.models import Lecture, LectureChapter
 from .models import UploadFile_summary
 from .forms import UploadFile_summaryForm  # UploadFileForm을 가져옴
+from django.urls import reverse
 
 # Create your views here.
+
+
+# def summary_detail_view(request, lecture_name, chapter_name):
+#     # 강의명과 챕터명이 일치하는 LectureChapter 객체를 가져옴
+#     chapter = LectureChapter.objects.filter(lecture__title=lecture_name, chapter_name=chapter_name).first()
+
+#     # 현재 로그인한 사용자 정보를 가져옴
+#     current_user = request.user
+
+#     # LectureChapter가 없는 경우 404 에러 반환
+#     if not chapter:
+#         raise Http404("챕터를 찾을 수 없습니다.")
+
+#     # 해당 챕터에 업로드된 파일들 가져오기
+#     uploaded_files = UploadFile_summary.objects.filter(chapter=chapter)
+
+#     # 파일 업로드를 위한 폼 생성
+#     form = UploadFile_summaryForm(request.POST or None, request.FILES or None)
+
+#     context = {
+#         'chapter': chapter,
+#         'uploaded_files': uploaded_files,  # 업로드된 파일들을 context에 추가
+#         'form': form,  # 폼을 context에 추가
+#     }
+
+#     return render(request, "summary/summary_detail.html", context)
+
 
 
 def summary_detail_view(request, lecture_name, chapter_name):
@@ -15,24 +43,48 @@ def summary_detail_view(request, lecture_name, chapter_name):
 
     # 현재 로그인한 사용자 정보를 가져옴
     current_user = request.user
+    lecture_chapters = LectureChapter.objects.filter(user=current_user).select_related('lecture').order_by('lecture__title')
 
     # LectureChapter가 없는 경우 404 에러 반환
     if not chapter:
         raise Http404("챕터를 찾을 수 없습니다.")
 
+    lectures = []
+    for chapter_obj in lecture_chapters:
+        lecture_title = chapter_obj.lecture.title
+        chapter_name = chapter_obj.chapter_name
+        lecture_url = reverse('user:lecture_detail', kwargs={'lecture_name': lecture_title})
+        chapter_url = reverse('upload:chapter_detail', kwargs={'lecture_name': lecture_title, 'chapter_name': chapter_name})
+
+        # 현재 강의가 lectures 리스트에 없으면 추가
+        if not any(lecture['lecture'] == lecture_title for lecture in lectures):
+            lectures.append({'lecture': lecture_title, 'chapters': []})
+
+        # 현재 챕터 추가
+        lectures[-1]['chapters'].append({'chapter_name': chapter_name, 'chapter_url': chapter_url, 'lecture_url': lecture_url})
+
     # 해당 챕터에 업로드된 파일들 가져오기
     uploaded_files = UploadFile_summary.objects.filter(chapter=chapter)
 
     # 파일 업로드를 위한 폼 생성
-    form = UploadFile_summaryForm(request.POST or None, request.FILES or None)
+    form = UploadFile_summaryForm(request.POST or None, request.FILES or None)    
+
 
     context = {
         'chapter': chapter,
+        'lectures': lectures,
+        'chapter_name': chapter_name,
         'uploaded_files': uploaded_files,  # 업로드된 파일들을 context에 추가
         'form': form,  # 폼을 context에 추가
     }
 
     return render(request, "summary/summary_detail.html", context)
+
+
+
+
+
+
 
 
 
