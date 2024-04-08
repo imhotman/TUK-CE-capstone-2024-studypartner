37e89e3d6c83d69f6feef2aa1c837c5402e0a823
seolhyebom <seolhyebom@gmail.com>
2024-04-08 22:53:49 +0900

    .

--- user/urls.py
+++ user/urls.py
@@ -14,15 +14,10 @@ urlpatterns = [
     path('timer/', views.timer_view, name='timer'),
     path('test1/', views.test1_view, name='test1'),
     path('test2/', views.test2_view, name='test2'),
-    path('update-session/', views.update_session_view, name='update_session'),  # /update-session/ URL에 대한 경로
+    path('update-session/', views.update_session_view, name='update_session'), # 새로운 URL 패턴 추가
     
     # lecture에 대한 URL 패턴
     path('lecture_detail/<str:lecture_name>/', views.lecture_detail_view, name='lecture_detail'),  # lecture_detail_view
     # chapter에 대한 URL 패턴
     # path('chapter_detail/<str:lecture_name>/<str:chapter_name>/', views.chapter_detail_view, name='chapter_detail'),  # chapter_detail_view  
 ]
-
-
-
-
-
