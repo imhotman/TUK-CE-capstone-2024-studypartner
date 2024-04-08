5a164c55463a36a2dae090769665d64d66a60982
seolhyebom <seolhyebom@gmail.com>
2024-04-08 11:24:33 +0900

    테스트 페이지 생성

--- upload/urls.py
+++ upload/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     # chapter에 대한 URL 패턴
     path('chapter_detail/<str:lecture_name>/<str:chapter_name>/', views.chapter_detail_view, name='chapter_detail'),  # chapter_detail_view
     path('timer/', views.timer_view, name='timer'),
-    path('test1/', views.test1_view, name='test1')
+    path('test1/', views.test1_view, name='test1'),
+    path('test2/', views.test2_view, name='test2'),
+
 
 ]
 
