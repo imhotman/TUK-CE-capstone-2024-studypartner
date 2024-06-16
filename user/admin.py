eceb7e5402ddf7c10cd7864f04cb070b304a7774
seolhyebom <seolhyebom@gmail.com>
2024-06-16 11:24:39 +0900

    친구시스템 업데이트

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Lecture, LectureChapter, Study_TimerSession
+from .models import Lecture, LectureChapter, Study_TimerSession, FriendRequest, Friendship
 
 admin.site.register(Lecture)
 admin.site.register(LectureChapter)
 admin.site.register(Study_TimerSession)
+# FriendRequest 모델 등록
+@admin.register(FriendRequest)
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_display = ['from_user', 'to_user', 'created_at']
+
+# Friendship 모델 등록
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ['user', 'friend', 'created_at']
