bfc49dc2c14a8ff43ef5a8ea99faddf49b1c8088
seolhyebom <seolhyebom@gmail.com>
2024-06-19 15:20:54 +0900

    손글씨 사이트 추가

--- summary/urls.py
+++ summary/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # chapter에 대한 URL 패턴
     path('summary_detail/<str:lecture_name>/<str:chapter_name>/', views.summary_detail_view, name='summary_detail'),  # summary_detail
     path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file_summary'),
-    path('delete_file/<int:file_id>/', views.delete_file_summary, name='delete_file_summary'),
+    path('delete_file_summary/<int:file_id>/', views.delete_file_summary, name='delete_file_summary'),
 
 ]
 
