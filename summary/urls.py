ab70b132be99ce580ff063c08b910e15008206b6
seolhyebom <seolhyebom@gmail.com>
2024-06-19 21:08:55 +0900

    AI 진짜요약 추가 업데이트

--- summary/urls.py
+++ summary/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file_summary'),
     path('delete_file_summary/<int:file_id>/', views.delete_file_summary, name='delete_file_summary'),
     path('stt_view/<int:file_id>/', views.stt_view, name='stt_view'),
+    path('show_summary_view/<int:file_id>/', views.show_summary_view, name='show_summary_view'),
     
 ]
 
