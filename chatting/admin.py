5987f4b81c56fba9eabe4561a8a25e8abdfb5041
seolhyebom <seolhyebom@gmail.com>
2024-07-28 00:39:16 +0900

    채팅 시스템 모델 추가

--- chatting/admin.py
+++ chatting/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import PrivateChatRoom, PrivateMessage
 
-# Register your models here.
+# PrivateChatRoom 모델을 관리할 관리자 클래스
+class PrivateChatRoomAdmin(admin.ModelAdmin):
+    # 관리자 페이지에서 보여줄 필드
+    list_display = ('user1', 'user2')
+    # 관리자 페이지에서 검색할 필드
+    search_fields = ('user1__username', 'user2__username')
+    # 관리자 페이지에서 필터링할 필드
+    list_filter = ('user1', 'user2')
+
+# PrivateMessage 모델을 관리할 관리자 클래스
+class PrivateMessageAdmin(admin.ModelAdmin):
+    # 관리자 페이지에서 보여줄 필드
+    list_display = ('chat_room', 'sender', 'receiver', 'content', 'file', 'timestamp', 'is_read')
+    # 관리자 페이지에서 검색할 필드
+    search_fields = ('sender__username', 'receiver__username', 'content')
+    # 관리자 페이지에서 필터링할 필드
+    list_filter = ('sender', 'receiver', 'chat_room', 'is_read')
+    # 관리자 페이지에서 필드 순서 지정
+    ordering = ('-timestamp',)  # 최근 메시지부터 정렬
+
+# 모델을 관리자 사이트에 등록
+admin.site.register(PrivateChatRoom, PrivateChatRoomAdmin)
+admin.site.register(PrivateMessage, PrivateMessageAdmin)
--- study/asgi.py
+++ study/asgi.py
@@ -7,10 +7,31 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eco.settings')
+
+# application = get_asgi_application()
+
+# asgi.py
+
+# study/asgi.py
+
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatting.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eco.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatting.routing.websocket_urlpatterns
+        )
+    ),
+})
