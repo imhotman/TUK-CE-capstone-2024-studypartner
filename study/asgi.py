f34c5a303cfeeddb8ab92a9084c1122789a9c29d
seolhyebom <seolhyebom@gmail.com>
2024-07-28 16:57:20 +0900

    채팅시스템 보안수정

--- study/asgi.py
+++ study/asgi.py
@@ -19,19 +19,46 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 # study/asgi.py
 
+
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
+# from channels.auth import AuthMiddlewareStack # type: ignore
+# from channels.security.websocket import AllowedHostsOriginValidator
+# import chatting.routing
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chatting.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
+
+
 import os
+
+from channels.auth import AuthMiddlewareStack # type: ignore
+from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
+from channels.security.websocket import AllowedHostsOriginValidator # type: ignore
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chatting.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'study.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chatting.routing.websocket_urlpatterns
-        )
-    ),
-})
+import chatting.routing
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chatting.routing.websocket_urlpatterns))
+        ),
+    }
+)
