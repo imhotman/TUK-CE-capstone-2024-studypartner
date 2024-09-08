d94c587e0c50cbc0ec9fcde5e6b902278d255704
seolhyebom <seolhyebom@gmail.com>
2024-09-09 01:50:52 +0900

    채팅시스템 및 업로드페이지 변경

--- summary/urls.py
+++ summary/urls.py
@@ -6,6 +6,8 @@ app_name = "summary"
 urlpatterns = [
     # chapter에 대한 URL 패턴
     path('summary_detail/<str:lecture_name>/<str:chapter_name>/', views.summary_detail_view, name='summary_detail'),  # summary_detail
+    path('AI_summarypage/<str:lecture_name>/<str:chapter_name>/', views.AI_summarypage_view, name='AI_summarypage'),  # AI_summarypage
+
     path('upload_file_summary/<str:lecture_name>/<str:chapter_name>/', views.upload_file_summary, name='upload_file_summary'),
     path('delete_file_summary/<int:file_id>/', views.delete_file_summary, name='delete_file_summary'),
     path('stt_view/<str:lecture_name>/<str:chapter_name>/<int:file_id>/', views.stt_view, name='stt_view'),
